f9cc4611f8d03ba1266eff927abb1a972a569a59
hsynkskn <151789235+hsynkskn@users.noreply.github.com>
2025-09-16 13:49:47 +0300

    Update yogurt1.py

--- yogurt1.py
+++ yogurt1.py
@@ -11,6 +11,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.memory import ConversationBufferMemory
 #from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.llms import HuggingFaceHub
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 
